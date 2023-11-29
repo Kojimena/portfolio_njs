8a3727faf2d7391a4d349d1a7430e94a5f360f7c
Jimena Hernández <her21199@uvg.edu.gt>
2023-11-28 18:18:41 -0600

    Update ProjectItem.jsx

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -8,7 +8,7 @@ const ProjectItem = ({img, link, name, desc}) => {
         <div className='relative flex flex-col items-start xs:justify-center md:justify-start gap-2 pt-2 md:p-4 w-full'>
             <span className="md:text-2xl font-hoey text-left text-white font-bold">{name}</span>
             <span className="md:text-2xl font-hoey text-left text-white ">{desc}</span>
-            <button className="md:absolute md:bottom-0 md:right-0 btn  xs:btn-xs bg-white text-black hover:bg-black hover:text-white">Ver proyecto</button>
+            <button className="md:absolute md:bottom-0 md:right-0 btn  xs:btn-xs bg-white text-black hover:bg-black hover:text-white" onClick={() => window.open(link, "_blank")}>Ver proyecto</button>
         </div>
     </div>
   )
